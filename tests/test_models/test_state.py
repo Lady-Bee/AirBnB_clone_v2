9ced8fd5376af505a7e51f42abb7ce2ab3fd5ae3
IfeneticTech <inehpaul@gmail.com>
2024-04-17 09:38:17 -0700

    Tasks for the AirBnB_Clone_v2 project

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-""" """
+"""The module that tests file: state.py
+"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """Class that tests state.py"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """The constructor for the test_state class."""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """more testing of the state name."""
         new = self.value()
         self.assertEqual(type(new.name), str)
