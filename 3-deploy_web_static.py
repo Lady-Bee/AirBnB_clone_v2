c1c8609c46534ea60f22c54790630e095e766fa4
Lady-Bee <bisimi16@gmail.com>
2024-05-04 16:09:22 -0700

    hbnb deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -21,7 +21,7 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception as e:
+    except:
         print(f"An error occurred: {e}")
         return None
 
