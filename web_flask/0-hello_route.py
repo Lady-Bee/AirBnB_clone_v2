05ebb73c2184f215c872e8234646b6fa12a674ee
Lady-Bee <bisimi16@gmail.com>
2024-06-20 03:30:13 -0700

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
-
+""" a script that starts a Flask web application """
 from flask import Flask
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def home():
-    """returns Hello HBNB!"""
+@app.route('/airbnb-onepage/')
+def hello_hbnb():
+    """prints web"""
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
